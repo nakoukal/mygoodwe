a36c3179b7b7e75e8efbc247d8600d4730afeeb1
Radek Nakoukal <radek@nakoukal.com>
2020-05-28 07:37:22 +0200

    -updated test

--- test.py
+++ test.py
@@ -4,22 +4,19 @@
 import json
 import time
 from mygoodwe import Goodwe
+from globals import *
 
 #params id,username,password
-gw = Goodwe('goodwe id','username','password')
-gw.call("true")
-print(gw.get_json_data())
+gw = Goodwe(SEMS_ID,SEMS_USR,SEMS_PWD)
 
-
-
-#running = True
-#while running:
-#	gw.call()
-#	print('dict')
-#	print(gw.get_dict_data())
-#	print('json')
-#	print(gw.get_json_data())
-#	print('formated output')
-#	gw.show()
-#	print("---------------------------")
-#	time.sleep(60)
+running = True
+while running:
+	gw.call()
+	print('dict')
+	print(gw.get_dict_data())
+	print('\n json')
+	print(gw.get_json_data())
+	print('\n formated output')
+	gw.show()
+	print("---------------------------")
+	time.sleep(60)
